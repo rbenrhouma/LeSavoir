b0bcbf54adf907721e253c6a1ce1ca2b1dbca1e5
rbenrhouma <rbenrhouma2@gmail.com>
2016-01-03 21:54:22 +0100

    .

--- LeSavoir/FichesAncetres/formeItem.java
+++ LeSavoir/FichesAncetres/formeItem.java
@@ -32,22 +32,34 @@ public class formeItem extends JFrame {
 	 * Create the frame.
 	 */
 	public formeItem() {
+		setTitle("New item");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 649, 364);
 		getContentPane().setLayout(null);
 		
 		JButton btnAnnuler = new JButton("Annuler");
-		btnAnnuler.setBounds(311, 222, 113, 29);
+		btnAnnuler.setIcon(null);
+		btnAnnuler.setBounds(490, 285, 113, 30);
 		getContentPane().add(btnAnnuler);
 		
 		JButton btnOk = new JButton("Ok");
-		btnOk.setBounds(212, 222, 89, 29);
+		btnOk.setIcon(new ImageIcon("C:\\Users\\Ridha\\Downloads\\Barre de navigation\\Valider.png"));
+		btnOk.setBounds(391, 285, 89, 30);
 		getContentPane().add(btnOk);
 		
 		JButton btnImprimer = new JButton("Imprimer");
 		btnImprimer.setIcon(new ImageIcon("C:\\Users\\Ridha\\Downloads\\icone_imprimante.png"));
-		btnImprimer.setBounds(10, 222, 113, 29);
+		btnImprimer.setBounds(10, 285, 103, 30);
 		getContentPane().add(btnImprimer);
+		
+		JButton btnNewButton = new JButton("Supprimer");
+		btnNewButton.setIcon(null);
+		btnNewButton.setBounds(133, 285, 121, 30);
+		getContentPane().add(btnNewButton);
+		
+		JButton btnEditer = new JButton("Editer");
+		btnEditer.setBounds(264, 285, 121, 30);
+		getContentPane().add(btnEditer);
 	}
 
 }
