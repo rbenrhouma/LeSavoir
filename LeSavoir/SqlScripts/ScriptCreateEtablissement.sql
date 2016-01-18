4381cac4b8a272d9cce9e42b810992682470e3cb
rbenrhouma <rbenrhouma2@gmail.com>
2016-01-18 22:38:33 +0100

    Script de création d'Etablissement.

---
+++ LeSavoir/SqlScripts/ScriptCreateEtablissement.sql
@@ -0,0 +1,6 @@
+
+EXEC sp_addextendedproperty @name = N'ETAB_ADRESSE_CODEPOSTAL'	, @value ='92390'
+EXEC sp_addextendedproperty @name = N'ETAB_ADRESSE_NOM'	, @value ='Rue Edouard MANET'
+EXEC sp_addextendedproperty @name = N'ETAB_ADRESSE_NUM'	, @value ='17-19'
+EXEC sp_addextendedproperty @name = N'ETAB_ADRESSE_VILLE'	, @value ='Villeneuve la garenne'
+EXEC sp_addextendedproperty @name = N'ETAB_NAME'	, @value ='Ecole le SAVOIR'
