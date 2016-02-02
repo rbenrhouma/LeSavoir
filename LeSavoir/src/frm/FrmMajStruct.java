9c906ba8470080d66c58c2ec38d4376730dbf8d2
rbenrhouma <rbenrhouma2@gmail.com>
2016-02-03 00:13:28 +0100

    Nouveau document.

--- LeSavoir/src/frm/FrmMajStruct.java
+++ LeSavoir/src/frm/FrmMajStruct.java
@@ -11,17 +11,19 @@ import javax.swing.SwingConstants;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JProgressBar;
-import java.awt.SystemColor;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.LayoutStyle.ComponentPlacement;
-import java.awt.Color;
-import java.awt.Font;
 import javax.swing.JScrollPane;
+
+import java.awt.SystemColor;
+import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.sql.*;
 import net.proteanit.sql.DbUtils;
+
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
+import javax.swing.ListSelectionModel;
 
 
 public class FrmMajStruct extends JFrame {
@@ -54,98 +56,16 @@ public class FrmMajStruct extends JFrame {
 		//connection=SQLIntegrityConstraintViolationException
 		setTitle("Mise \u00E0 jour de la strucure de la base de donn\u00E9e");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1001, 567);
+		setBounds(100, 100, 1200, 629);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new GridLayout(0, 3, 1, 1));
-		
-		JScrollPane scrollPane = new JScrollPane();
-		contentPane.add(scrollPane);
-
-		JPanel panel_1 = new JPanel();
-		scrollPane.setViewportView(panel_1);
-
-		JPanel panel_5 = new JPanel();
-		panel_5.setBackground(SystemColor.textHighlight);
-		
-		t_table = new JTable();
-		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
-		gl_panel_1.setHorizontalGroup(
-			gl_panel_1.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_1.createSequentialGroup()
-					.addGap(13)
-					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
-						.addComponent(t_table, GroupLayout.PREFERRED_SIZE, 288, GroupLayout.PREFERRED_SIZE)
-						.addComponent(panel_5, GroupLayout.DEFAULT_SIZE, 306, Short.MAX_VALUE))
-					.addContainerGap())
-		);
-		gl_panel_1.setVerticalGroup(
-			gl_panel_1.createParallelGroup(Alignment.TRAILING)
-				.addGroup(Alignment.LEADING, gl_panel_1.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(panel_5, GroupLayout.PREFERRED_SIZE, 47, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(t_table, GroupLayout.PREFERRED_SIZE, 215, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(233, Short.MAX_VALUE))
-		);
-		panel_5.setLayout(new GridLayout(2, 1, 0, 0));
-
-		JLabel Tables = new JLabel("Tables");
-		Tables.setFont(new Font("Tahoma", Font.BOLD, 16));
-		Tables.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_5.add(Tables);
-
-		JLabel lblNewLabel = new JLabel(" Liste des Tables \u00E0 mettre \u00E0 jour");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.LEFT);
-		panel_5.add(lblNewLabel);
-		panel_1.setLayout(gl_panel_1);
-		
-		JScrollPane scrollPane_1 = new JScrollPane();
-		contentPane.add(scrollPane_1);
-
-		JPanel panel_2 = new JPanel();
-		scrollPane_1.setViewportView(panel_2);
-
-		JPanel panel_3 = new JPanel();
-		panel_3.setBackground(SystemColor.activeCaption);
-		
-		t_Fields = new JTable();
-		GroupLayout gl_panel_2 = new GroupLayout(panel_2);
-		gl_panel_2.setHorizontalGroup(
-			gl_panel_2.createParallelGroup(Alignment.TRAILING)
-				.addGroup(Alignment.LEADING, gl_panel_2.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(gl_panel_2.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_panel_2.createSequentialGroup()
-							.addGap(6)
-							.addComponent(t_Fields, GroupLayout.PREFERRED_SIZE, 290, GroupLayout.PREFERRED_SIZE))
-						.addComponent(panel_3, GroupLayout.DEFAULT_SIZE, 306, Short.MAX_VALUE))
-					.addContainerGap())
-		);
-		gl_panel_2.setVerticalGroup(
-			gl_panel_2.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_2.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(panel_3, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(t_Fields, GroupLayout.PREFERRED_SIZE, 322, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(128, Short.MAX_VALUE))
-		);
-		panel_3.setLayout(new GridLayout(2, 1, 0, 0));
-
-		JLabel lblFields = new JLabel("Champs");
-		lblFields.setHorizontalAlignment(SwingConstants.CENTER);
-		lblFields.setFont(new Font("Tahoma", Font.BOLD, 16));
-		panel_3.add(lblFields);
-
-		JLabel lblNewLabel_1 = new JLabel(" Liste des champs \u00E0 mettre \u00E0 jour");
-		panel_3.add(lblNewLabel_1);
-		panel_2.setLayout(gl_panel_2);
+		contentPane.setLayout(null);
 
 		JPanel panel = new JPanel();
+		panel.setBounds(1028, 6, 150, 519);
 		contentPane.add(panel);
-		panel.setLayout(new GridLayout(10, 1, 0, 0));
+		panel.setLayout(new GridLayout(0, 1, 0, 10));
 
 		JButton btnNewButton = new JButton("Chargement");
 		btnNewButton.addActionListener(new ActionListener() {
@@ -154,13 +74,39 @@ public class FrmMajStruct extends JFrame {
 						String query = " SELECT * FROM TABLES ";
 						PreparedStatement pst = connection.prepareStatement(query);
 						ResultSet rs = pst.executeQuery();
-						t_table.setModel(DbUtils.resultSetToTableModel(rs));					
-
+						t_table.setModel(DbUtils.resultSetToTableModel(rs));		
+						t_table.getColumnModel().getColumn(0).setPreferredWidth(200);
+						t_table.getColumnModel().getColumn(1).setPreferredWidth(200);
+						t_table.getColumnModel().getColumn(2).setPreferredWidth(200);
+						t_table.getColumnModel().getColumn(3).setPreferredWidth(200);
+						
+						//TableColumnModel tcm = t_table.getColumnModel();
+						//TableColumn tc = tcm.getColumn(1);
+						//tc.setHeaderValue( "..." );
+						
+			            //JScrollPane pane = new JScrollPane(t_table);
+			            //add(pane);
+						
+						
+						t_table.setRowSelectionAllowed(true);
+						t_table.getTableHeader().setReorderingAllowed(true);
 						
 				} catch (Exception X) {
 					X.printStackTrace();
 				}				
 				
+				
+
+				try {
+					String query = " SELECT * FROM FIELDS ";
+					PreparedStatement pst2 = connection.prepareStatement(query);
+					ResultSet rs2 = pst2.executeQuery();
+					t_Fields.setModel(DbUtils.resultSetToTableModel(rs2));					
+
+					
+			} catch (Exception X) {
+				X.printStackTrace();
+			}				
 				
 				
 			}
@@ -168,27 +114,17 @@ public class FrmMajStruct extends JFrame {
 		btnNewButton.setIcon(new ImageIcon(FrmMajStruct.class.getResource("/Images/load-download-icon.png")));
 		panel.add(btnNewButton);
 
-		JPanel panel_7 = new JPanel();
-		panel.add(panel_7);
-
 		JPanel panel_8 = new JPanel();
 		panel.add(panel_8);
+		
+				JPanel panel_7 = new JPanel();
+				panel_8.add(panel_7);
 
 		JButton btnNewButton_2 = new JButton("");
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				
-
-				try {
-					String query = " SELECT * FROM FIELDS ";
-					PreparedStatement pst = connection.prepareStatement(query);
-					ResultSet rs = pst.executeQuery();
-					t_Fields.setModel(DbUtils.resultSetToTableModel(rs));					
-
-					
-			} catch (Exception X) {
-				X.printStackTrace();
-			}				
+				
 			
 			
 			
@@ -214,7 +150,60 @@ public class FrmMajStruct extends JFrame {
 		panel.add(btnNewButton_1);
 
 		JProgressBar progressBar = new JProgressBar();
-		progressBar.setValue(90);
+		progressBar.setValue(20);
 		panel.add(progressBar);
+		
+				JPanel panel_1 = new JPanel();
+				panel_1.setBounds(6, 6, 1010, 518);
+				contentPane.add(panel_1);
+				panel_1.setLayout(null);
+				
+						JPanel panel_5 = new JPanel();
+						panel_5.setBounds(0, 0, 1008, 40);
+						panel_5.setBackground(SystemColor.textHighlight);
+						panel_5.setLayout(new GridLayout(2, 1, 0, 0));
+						
+								JLabel Tables = new JLabel("Tables");
+								Tables.setFont(new Font("Tahoma", Font.BOLD, 16));
+								Tables.setHorizontalAlignment(SwingConstants.CENTER);
+								panel_5.add(Tables);
+								
+										JLabel lblNewLabel = new JLabel(" Liste des Tables \u00E0 mettre \u00E0 jour");
+										lblNewLabel.setHorizontalAlignment(SwingConstants.LEFT);
+										panel_5.add(lblNewLabel);
+										panel_1.add(panel_5);
+										
+										t_table = new JTable();
+										t_table.setShowVerticalLines(true);
+										t_table.setShowHorizontalLines(true);
+										t_table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+										t_table.setModel(new DefaultTableModel(
+											new Object[][] {
+											},
+											new String[] {
+												"vbvcb", "New column", "mmmm", "New column", "New column", "New column", "New column"
+											}
+										));
+										t_table.setBounds(0, 45, 1008, 215);
+										panel_1.add(t_table);
+										
+										t_Fields = new JTable();
+										t_Fields.setBounds(0, 323, 1008, 177);
+										panel_1.add(t_Fields);
+										
+										JPanel panel_2 = new JPanel();
+										panel_2.setBackground(SystemColor.textHighlight);
+										panel_2.setBounds(0, 271, 1008, 40);
+										panel_1.add(panel_2);
+										panel_2.setLayout(new GridLayout(2, 1, 0, 0));
+										
+										JLabel lblChamps = new JLabel("Champs");
+										lblChamps.setHorizontalAlignment(SwingConstants.CENTER);
+										lblChamps.setFont(new Font("Tahoma", Font.BOLD, 16));
+										panel_2.add(lblChamps);
+										
+										JLabel lblListeDesChamps = new JLabel(" Liste des Champs \u00E0 mettre \u00E0 jour");
+										lblListeDesChamps.setHorizontalAlignment(SwingConstants.LEFT);
+										panel_2.add(lblListeDesChamps);
 	}
 }
